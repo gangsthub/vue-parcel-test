080d7bc72f85213767e4356d8649337edcab5a59
paul_melero <paulmelero@gmail>
2019-02-18 20:32:08 +0100

    [master] init

---
+++ App.vue
@@ -0,0 +1,12 @@
+<template>
+  <search-box></search-box>
+</template>
+
+<script>
+import SearchBox from './components/search.vue'
+export default {
+  components: {
+    SearchBox
+  }
+}
+</script>
---
+++ main.js
@@ -0,0 +1,9 @@
+
+import Vue from 'vue/dist/vue.min.js'
+import App from './App'
+
+new Vue({
+  el: '#app-sample',
+  // render: h => h(App)
+  template: '<App/>'
+})
